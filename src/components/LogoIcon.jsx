e88093ceb2bd2804cb7680c1b3b65be23007a1ad
nipunsw1999 <nipunsw1999@gmail.com>
2025-07-24 10:32:49 +0530

    ui fixed

--- src/components/LogoIcon.jsx
+++ src/components/LogoIcon.jsx
@@ -1,7 +1,7 @@
 const LogoIcon = () => {
   return (
     <img
-      src="/h2o.svg" // ✅ relative to the public/ folder
+      src="https://h2o.ai/company/brand-kit/_jcr_content/root/container/section/par/advancedcolumncontro/columns1/advancedcolumncontro/columns0/image.coreimg.svg/1710457468387/h2o-logo.svg" // ✅ relative to the public/ folder
       alt="H2O Logo"
       width="50"
       height="50"
